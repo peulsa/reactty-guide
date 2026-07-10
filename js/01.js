08596144ca073cd4eda91442211ed9bd4bd40327
peulsa <erikka022003@gmail.com>
2026-07-10 18:33:43 -0400

    Diferencia de variables con CONST y LET con ejemplos

---
+++ js/01.js
@@ -0,0 +1,7 @@
+let cliente = "Juan"
+cliente = 30
+console.log(cliente)
+
+let precio
+precio = 1000
+console.log(precio)
---
+++ js/02.js
@@ -0,0 +1,6 @@
+const cliente = "Juan"
+
+console.log(cliente)
+
+const precio = 1000 //se inicializa con un = para definir el vaalor, no puede ser una variable suelta o cambiante
+console.log(precio) 
