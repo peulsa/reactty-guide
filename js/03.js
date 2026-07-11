2a3b2490e1ad0c405b2edec095bd4f8de1abcd19
peulsa <erikka022003@gmail.com>
2026-07-10 21:57:31 -0400

    Tipos de datos

---
+++ js/03.js
@@ -0,0 +1,54 @@
+// Comentario
+
+/* comentario varias
+lineas
+*/
+
+
+//Undefined
+
+//let cliente
+//console.log(cliente)
+//console.log(typeof cliente) para mostrar el tipo de dato 
+
+//string o cadenas de texto
+//const alumno = "juan"
+//console.log(alumno)
+//console.log(typeof alumno)   
+
+//const producto = 'Monitor 49'
+//console.log(producto)
+//console.log(typeof producto)
+
+//Numbers
+// const nro1 = 20.20
+// const nro2 = 20
+// const nro3 = -100
+// const nro4 = "-100"
+
+// console.log(typeof nro1)
+// console.log(typeof nro2)
+// console.log(typeof nro3)
+// console.log(typeof nro4)
+
+//BigInt
+// const nroGrande = BigInt(100000000000000000000)
+// console.log(typeof nroGrande)
+
+// //Boolean si o no
+// const descuento = false
+// console.log(typeof descuento)
+
+//Null 
+// const descuento = null
+// console.log(descuento)
+// let precio = undefined
+// console.log(precio)
+
+//Symbol
+const primerSybol=Symbol(30)
+const segundoSymbol=Symbol(30)
+
+console.log(primerSybol)
+
+console.log(primerSybol === segundoSymbol)
