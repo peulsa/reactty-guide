35b9938e5bd5c8569bf69d22c7134d2162fd454f
peulsa <erikka022003@gmail.com>
2026-07-10 22:19:40 -0400

    Objetos y destructuring

---
+++ js/04.js
@@ -0,0 +1,32 @@
+// //Objetos
+// //propiedades, nombre = key y Tablet = value
+// const producto = {
+//     nombre : "Tablet", 
+//     precio : 300,
+//     disponible : false
+// }
+
+// // // console.log(producto)
+// // // console.log(typeof producto)
+
+// // // console.table(producto)
+
+// // console.log(producto.nombre); //sintaxis de punto en los objetos
+
+
+// //Destructuring
+// // const { nombre, precio, disponible } = producto
+// // console.log(nombre)
+// // console.log(precio)
+// // console.log(disponible)
+
+
+// //Object Literal Enhancement
+// const autenticado = true
+// const usuario = "Juan"
+
+// const nuevoObjeto = {
+//     autenticado : autenticado,//si se llama igual la key y value solo llamo a uno ->  autenticado, usuario
+//     usuario : usuario
+// }
+// console.log(nuevoObjeto)
