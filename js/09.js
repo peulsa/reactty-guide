b199f866bf6c39f01821fe691ad3fa07377b9a91
peulsa <erikka022003@gmail.com>
2026-07-13 23:05:17 -0400

    Arrays o Arreglos (listado)

---
+++ js/09.js
@@ -0,0 +1,7 @@
+//Arrays o Arreglos (listado) va en corchetes [] ya que solo tienen valores y se asigna por posición o índice
+
+const tecnologias = [20, 30, 40, true, "React.js"] //idealmente tener un arreglo para cada categoria
+console.log(tecnologias)
+console.table(tecnologias)//tabla
+
+console.log(tecnologias[0])//para acceder a uno en específico
