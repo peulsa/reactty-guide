5262be01b74faaba6eda08baf2568fdeee306e88
peulsa <erikka022003@gmail.com>
2026-07-17 20:33:16 -0400

    Funciones y pq hago funciones deben se con algún sentido chiquillo estupido

---
+++ js/13.js
@@ -0,0 +1,9 @@
+// Function declaration - Repite código para reciclar
+
+function sumar(numero1 = 2, numero2 = 3 ){
+    console.log( numero1 )
+    console.log( numero2 )
+    console.log( numero1 + numero2 )
+}
+
+sumar(10) //hay q llamar la función
